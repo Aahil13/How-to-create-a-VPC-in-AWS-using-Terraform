878cf3ab0a51b9fdac11ba8ecd024db57480dc95
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-05-23 00:02:33 +0100

    initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "availablity_zone1" {
+  type = string
+  default = "us-east-1a"
+}
+
+variable "availablity_zone2" {
+  type = string
+  default = "us-east-1b"
+}

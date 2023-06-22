0fb249ca1cff31ba2d904a9446636114d6766e4d
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-06-22 09:44:47 +0100

    Update: "Include necessary variables"

--- variables.tf
+++ variables.tf
@@ -1,14 +1,41 @@
+######################################################################################
+#                                       VARIABLES
+######################################################################################
+
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "availablity_zone1" {
-  type = string
+  type    = string
   default = "us-east-1a"
 }
 
 variable "availablity_zone2" {
-  type = string
+  type    = string
   default = "us-east-1b"
 }
+
+variable "keypair" {
+  type    = string
+  default = "altschool"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "image_id" {
+  type = string
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "cidr_block" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+
+

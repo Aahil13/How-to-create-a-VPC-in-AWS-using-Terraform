e1f932e8541fcde9551c250276166e02bc971f4f
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-06-22 09:44:10 +0100

    Update: "Add Securiy Groups"

---
+++ SECURITY.tf
@@ -0,0 +1,88 @@
+######################################################################################
+#                                   SECURITY GROUPS
+######################################################################################
+
+# EC2 Security Group
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = aws_vpc.demo_vpc.id
+}
+
+resource "aws_security_group_rule" "ingress_HTTP" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "http"
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_blocks       = [var.cidr_block]
+}
+
+resource "aws_security_group_rule" "ingress_SSH" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "ssh"
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_blocks       = [var.cidr_block]
+}
+
+######################################################################################
+######################################################################################
+
+
+# Auto Scaling Security Group
+resource "aws_security_group" "asg_sg" {
+  name   = "asg-sg"
+  vpc_id = aws_vpc.demo_vpc.id
+
+  ingress {
+    description     = "Allow traffic from only ALB"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    description = "Allow traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.cidr_block]
+  }
+}
+
+######################################################################################
+######################################################################################
+
+
+# Application Load Balancer Security Group
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = aws_vpc.demo_vpc.id
+
+  ingress {
+    description = "Traffic from HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  ingress {
+    description = "Traffic from HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  egress {
+    description = "All Traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.cidr_block]
+  }
+}
+

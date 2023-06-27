b93574f8f3d9b8b4453d46e2fecf3d143886ec9a
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-06-27 01:02:27 +0100

    Update: "Fix ASG bug"

--- ASG.tf
+++ ASG.tf
@@ -2,13 +2,13 @@
 #                         AUTO SCALING GROUP CONFIGURATION
 ######################################################################################
 
-resource "aws_launch_template" "demo_instance_auto" {
-  name_prefix          = "demo_instance_auto"
-  image_id             = var.image_id
-  instance_type        = var.instance_type
-
-  key_name  = var.keypair
-  user_data = base64encode(file("apache.sh"))
+resource "aws_launch_configuration" "demo_instance_auto" {
+  name            = "demo_instance_auto"
+  image_id        = var.image_id
+  instance_type   = var.instance_type
+  key_name        = var.keypair
+  security_groups = [aws_security_group.ec2_sg.id]
+  user_data       = base64encode(file("apache.sh"))
 }
 
 resource "aws_autoscaling_group" "asg" {
@@ -18,12 +18,10 @@ resource "aws_autoscaling_group" "asg" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = true
+  depends_on                = [aws_launch_configuration.demo_instance_auto]
   vpc_zone_identifier       = [aws_subnet.private_subnet_1a.id, aws_subnet.private_subnet_1b.id]
-
-  launch_template {
-    id      = aws_launch_template.demo_instance_auto.id
-    version = "$Latest"
-  }
+  launch_configuration      = aws_launch_configuration.demo_instance_auto.id
+  target_group_arns         = [aws_lb_target_group.alb_tg.arn]
 
   timeouts {
     delete = "15m"
